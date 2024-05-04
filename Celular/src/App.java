be93c65c01c39be4c9911adc9095ffd3f4f6fbce
lucasalves0722 <lucasalves098messias@gmail.com>
2024-05-04 17:59:45 -0300

    declarando e instanciando um objeto

--- Celular/src/App.java
+++ Celular/src/App.java
@@ -13,5 +13,21 @@ public class App {
         celularA.tamanhoTela = 6.1F;
         celularA.espacoArmazenamento = 256;
         celularA.sistemaOperacional = "iOS";
+
+        Celular celularB = new Celular();
+        celularB.nome = "Galaxy Note 20 Ultra";
+        celularB.tamanhoTela = 6.9F;
+        celularB.espacoArmazenamento = 256;
+        celularB.sistemaOperacional = "Android";
+
+        Celular celularC = new Celular();
+        celularC.nome = "Xiaomi M1 11 Pro";
+        celularC.tamanhoTela = 6.81F;
+        celularC.espacoArmazenamento = 128;
+        celularC.sistemaOperacional = "Android";
+
+        System.out.format("Celular %s com tela de %.1f, com %dgb e SO %s\n", celularA.nome, celularA.tamanhoTela, celularA.espacoArmazenamento, celularA.sistemaOperacional);
+        System.out.format("Celular %s com tela de %.1f, com %dgb e SO %s\n", celularB.nome, celularB.tamanhoTela, celularB.espacoArmazenamento, celularB.sistemaOperacional);
+        System.out.format("Celular %s com tela de %.2f, com %dgb e SO %s\n", celularC.nome, celularC.tamanhoTela, celularC.espacoArmazenamento, celularC.sistemaOperacional);
     }
 }
